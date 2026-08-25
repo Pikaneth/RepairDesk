1f549672c245d52d56a997c2f4521aaed9d97126
Sviatoslav <81773068+Pikaneth@users.noreply.github.com>
2026-08-25 18:55:10 +0300

    Connect production Supabase project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.REPAIRDESK_CONFIG = Object.freeze({
-  supabaseUrl: "",
-  supabasePublishableKey: "",
+  supabaseUrl: "https://krnuetmthjskgtuttkhv.supabase.co",
+  supabasePublishableKey: "sb_publishable_cgXtSqyXkDppSlVqmTDqcA_F9A6QBbE",
   siteUrl: window.location.origin + window.location.pathname,
   analyticsEnabled: true,
 });
